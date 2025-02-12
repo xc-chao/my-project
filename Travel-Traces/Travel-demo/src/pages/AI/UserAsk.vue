2621cafa6e7402278c2272930371a42ce44e332c
xc-chao <2780036289@qq.com>
2025-02-12 23:26:49 +0800

    AI助手流程

---
+++ Travel-Traces/Travel-demo/src/pages/AI/UserAsk.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex py-2">
+    <div
+      class="relative rounded-lg bg-violet-200 border border-gray-300 px-4 py-2 ml-auto mr-4 text-gray-700 max-w-56"
+    >
+      <div class="avatar absolute top-2 -right-7">
+        <van-icon name="user" size="1.5rem" color="#66666"/>
+      </div>
+      <p v-html="props.msg"></p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+   msg: string;
+}
+const props = defineProps<Props>();
+</script>
+
+<style lang="less" scoped>
+/* Add any additional styles here if needed */
+</style>
