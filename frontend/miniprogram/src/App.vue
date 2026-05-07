f0574bb1f9dfc9fea9cfd416479263be73106e92
xc-chao <2780036289@qq.com>
2026-05-07 15:09:33 +0800

    feat：修复样式

--- frontend/miniprogram/src/App.vue
+++ frontend/miniprogram/src/App.vue
@@ -11,4 +11,23 @@ page {
   color: #111111;
   font-family: 'Inter', sans-serif;
 }
+
+view,
+text,
+button,
+scroll-view,
+swiper,
+swiper-item,
+image,
+input,
+textarea,
+switch,
+checkbox,
+radio,
+label,
+form,
+movable-area,
+movable-view {
+  box-sizing: border-box;
+}
 </style>
